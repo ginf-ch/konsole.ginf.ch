f52bf881ef60bb86075386a014ddbad4034ef5dc
pro-kswe <pro@kswe.ch>
2024-02-16 12:01:22 +0100

    feat: initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Konsolen-Programmierung | konsole.ginf.ch</title>
+    <?php
+    include("head.php");
+    ?>
+</head>
+<body>
+<header>
+    <?php
+    include("header.html");
+    ?>
+</header>
+<main>
+    <section  class="section is-medium has-text-centered">
+       <img src="images/coming_soon.png" alt="Coming Soon">
+    </section>
+</main>
+<footer class="footer">
+    <?php
+    include("footer.php");
+    ?>
+</footer>
+</body>
+</html>
